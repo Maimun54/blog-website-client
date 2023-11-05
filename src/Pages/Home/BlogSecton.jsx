c5a14143b5a2a7d4e5d9a66e9f3f804f8620b16e
Maimun54 <maimunislam@gmail.com>
2023-11-05 12:39:20 +0600

    Recent blog section added on Home page

--- src/Pages/Home/BlogSecton.jsx
+++ src/Pages/Home/BlogSecton.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
 
 
 const BlogSecton = () => {
+     
+    const [recentBlog,setRecentBlog]=useState([])
+      console.log(recentBlog)
+    useEffect(()=>{
+           fetch('http://localhost:5000/recentBlog')
+           .then(res=>res.json())
+           .then(data=>setRecentBlog(data))
+    },[])
     return (
         <div>
-            <h2 className="text-4xl text-center font-bold lg:py-20">Our Recent Blog</h2>
+        <h2 className="text-4xl text-center font-bold lg:py-20">Our Recent Blog</h2>
+         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-10 ">
+         {
+            recentBlog.map(recent=><div key={recent._id}>
+        <div className="card h-[500px] card-compact  bg-base-100 shadow-xl">
+  <figure><img className="w-full h-[300px]" src={recent.image} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">{recent.title}</h2>
+    <p className="text-sm" >{recent.description}</p>
+    <h2 className="text-2xl py-2 font-medium">Category:{recent.category}</h2>
+    <div className="card-actions justify-between">
+      <button className="btn btn-primary">Details</button>
+      <button className="btn btn-primary">WishList</button>
+    </div>
+  </div>
+</div>
+            </div>)
+         }
+         </div>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,17 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "./Banner";
 import BlogSecton from "./BlogSecton";
 
 
 const Home = () => {
+    const recentBlog =useLoaderData();
+    
+
     return (
         <div>
             <Banner></Banner>
             <BlogSecton></BlogSecton>
+            <h2>BLog:{recentBlog.length}</h2>
         </div>
     );
 };
